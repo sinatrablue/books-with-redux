8a6cf9ad705845cc6d8d63e5140f2920dca3288c
Robin Simonot <simonot.robin@gmail.com>
2023-07-16 16:51:23 +0200

    Introduced store the modern way

--- src/store/actions/actionsTypes.ts
+++
@@ -1,5 +0,0 @@
-export const actionsTypes = {
-  ADD_BOOK: "ADD_BOOK",
-  DELETE_BOOK: "DELETE_BOOK",
-  EDIT_BOOK: "EDIT_BOOK",
-};
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,5 @@
-import { Store, applyMiddleware, createStore } from "redux";
-import reducer from "./reducers/reducer";
-import thunk from "redux-thunk";
+import { configureStore } from "@reduxjs/toolkit";
 
-export const store: Store<BookState, BookAction> & {
-  dispatch: DispatchType;
-} = createStore(reducer, applyMiddleware(thunk));
+export default configureStore({
+  reducer: {},
+});
